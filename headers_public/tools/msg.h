84bed3ec506970c99e043ae4a6d73faf380c20fa
Benoit Hamon <benoit.hamon@epitech.eu>
2016-12-04 14:13:34 +0100

    Correct public header : msg.h

--- headers_public/tools/msg.h
+++ headers_public/tools/msg.h
@@ -5,20 +5,19 @@
 ** Login   <hamon_e@epitech.net>
 **
 ** Started on  Sun Dec 04 12:55:15 2016 Benoit Hamon
-** Last update Sun Dec 04 12:55:46 2016 Benoit Hamon
+** Last update Sun Dec 04 14:13:11 2016 Benoit Hamon
 */
 
 #ifndef HEADERS_PUBLIC_TOOLS_MSG_H_
 # define HEADERS_PUBLIC_TOOLS_MSG_H_
 
-# define THE_MSG_PATH		"../../../../include/tools/the_msg.h"
+# define THE_MSG_PATH		"../../../headers/tools/msg.h"
 
 # define ERR_MSG_OPEN		"Could not open: %s\n"
 # define ERR_MSG_READ		"Could not read: %s\n"
 # define ERR_MSG_MALLOC		"Malloc Failed\n"
-# define ERR_MSG_INFINITE_LOOP	"Do you have an infinite loop ?\n"
-# if __has_include(THE_FREE_ENUM_PATH)
-#  include THE_FREE_ENUM_PATH
+# if __has_include(THE_MSG_PATH)
+#  include THE_MSG_PATH
 # endif
 
 #endif /* end of include guard: HEADERS_PUBLIC_TOOLS_MSG_H_ */
